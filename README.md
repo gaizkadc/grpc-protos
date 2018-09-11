6b755fab583532305f72907ab0d52bda3acfe8f5
Daniel Higuero <daniel@daisho.group>
2018-09-11 15:02:23 +0200

    NP-111 Initial skeleton for the protobuf monorepo

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # grpc-protos
 Common repository to store all entities and service definitions for Nalej components using gRPC
+
+# Structure
+
+All the services and messages definitions for the Nalej components interacting through gRPC are defined in this repository. The organization of the repository matches the following example:
+
+```
+grpc-protos
+├── errors
+│   ├── .protolangs
+│   └── errors.proto
+├── organizations
+│   ├── .protolangs
+│   └── organizations.proto
+└── clusters
+    ├── .protolangs
+    ├── entities.proto
+    └── services.proto
+```
+
+The basic structure correspond to creating a directory per service. Naming of the files inside each service is expected to contain the name of entities or be separated in several files if the contents are no longer maintainable (e.g., entities.proto and services.proto).
+
+On each service, a file named **.protolangs** needs to be created specifying which are the target languages for the code generation phase. For example:
+
+```
+$ cat .protolangs
+go
+```
+
+Once the services are ready, the publish script will generate the appropiate code and push the changes to the related repository. Target repositories are named as `grpc-$service-$lang`
+
+## Adding a new service
+
+The following steps are required to add a new service. For example, assuming we want to create hello service in Nalej the following steps are required:
+
+1. Create a new directory `mkdir hello`
+2. Create a simple file `hello/hello.proto` with the message/service definition.
+3. Create a `hello/.protolangs` file specifying go a s the language.
+4. Create the target repository in GitHub (i.e., `grpc-hello-go`) with an initial commit
+5. Commit and push the changes of `grpc-protos` and the CI/CD system will auto generate the code and publish it.
+6. Alternatively, manually run the publish script.
+
+## Checking correctness
+
+You can check the code style rules with the following command:
+
+```
+protoc --lint_out=. */*.proto
+```
+
+# Autogenerating client/server stubs
+
+The repository contains a Bash script `publishProto.sh` that automatically generate the gRPC client/server stubs and publish them to the appropiate repository for component integration. To generate those the following environment variables are expected:
+
+* CURRENT_BRANCH: Working branch that will be used to pull/push changes to the client/server repositories.
+* REPOPATH: The local path of the grpc-protos repository
+
+For local manual pushes use:
+
+```
+$ CURRENT_BRANCH=master REPOPATH=~/go/src/github.com/nalej/grpc-protos ./publishProto.sh
+```
---
+++ publishProto.sh
@@ -0,0 +1,132 @@
+#!/usr/bin/env bash
+# This script generates the grpc clients for a set of services. Code based
+# on the namely gist available at: https://gist.githubusercontent.com/bobbytables/2fab9ac9509867b5acfe2bb5693aee71/raw/6f6d9cf27facd05fc88762ed7ab78cac7e72124d/build.sh
+
+# Original description:
+# This script is meant to build and compile every protocolbuffer for each
+# service declared in this repository (as defined by sub-directories).
+# It compiles using docker containers based on Namely's protoc image
+# seen here: https://github.com/namely/docker-protoc
+
+set -e
+
+REPOPATH=${REPOPATH-/opt/protolangs}
+echo "Repopath: ${REPOPATH}"
+CURRENT_BRANCH=${CURRENT_BRANCH-"branch-not-available"}
+echo "CurrentBranch: ${CURRENT_BRANCH}"
+
+# Helper for adding a directory to the stack and echoing the result
+function enterDir {
+  echo "Entering $1"
+  pushd $1 > /dev/null
+}
+
+# Helper for popping a directory off the stack and echoing the result
+function leaveDir {
+  echo "Leaving `pwd`"
+  popd > /dev/null
+}
+
+# Enters the directory and starts the build / compile process for the services
+# protobufs
+function buildDir {
+  currentDir="$1"
+  echo "Building directory \"$currentDir\""
+
+  enterDir $currentDir
+
+  buildProtoForTypes $currentDir
+
+  leaveDir
+}
+
+# Iterates through all of the languages listed in the services .protolangs file
+# and compiles them individually
+function buildProtoForTypes {
+  target=${1%/}
+
+  if [ -f .protolangs ]; then
+    while read lang; do
+      reponame="grpc-$target-$lang"
+      echo "Building $reponame"
+      rm -rf $REPOPATH/$reponame
+
+      echo "Cloning repo: git@github.com:nalej/$reponame.git"
+
+      # Clone the repository down and set the branch to the automated one
+      git clone git@github.com:nalej/$reponame.git $REPOPATH/$reponame
+      setupBranch $REPOPATH/$reponame
+
+      # Use the docker container for the language we care about and compile
+      docker run -v `pwd`:/defs namely/protoc-$lang
+
+      # Copy the generated files out of the pb-* path into the repository
+      # that we care about
+      cp -R pb-$lang/* $REPOPATH/$reponame/
+
+      commitAndPush $REPOPATH/$reponame
+    done < .protolangs
+  fi
+}
+
+# Finds all directories in the repository and iterates through them calling the
+# compile process for each one
+function buildAll {
+  echo "Buidling service's protocol buffers"
+  mkdir -p $REPOPATH
+  for d in */; do
+    buildDir $d
+  done
+}
+
+function setupBranch {
+  enterDir $1
+
+  echo "Creating branch"
+
+  if ! git show-branch $CURRENT_BRANCH; then
+    git branch $CURRENT_BRANCH
+  fi
+
+  git checkout $CURRENT_BRANCH
+
+  if git ls-remote --heads --exit-code origin $CURRENT_BRANCH; then
+    echo "Branch exists on remote, pulling latest changes"
+    git pull origin $CURRENT_BRANCH
+  fi
+
+  leaveDir
+}
+
+function commitAndPush {
+  enterDir $1
+
+  git add -N .
+
+  if ! git diff --exit-code > /dev/null; then
+    if [ ! -f VERSION ]; then
+        echo "Creating initial version"
+        echo "0.0.0" > VERSION
+    fi
+    CURRENT_VERSION_STRING=`cat VERSION`
+    VERSION_VALUES=(`echo $CURRENT_VERSION_STRING | tr '.' ' '`)
+    V_MAJOR=${VERSION_VALUES[0]}
+    V_MINOR=${VERSION_VALUES[1]}
+    V_PATCH=${VERSION_VALUES[2]}
+    V_PATCH=$((V_PATCH + 1))
+    NEW_VERSION="${V_MAJOR}.${V_MINOR}.${V_PATCH}"
+    echo "New version will be ${NEW_VERSION}"
+    echo $NEW_VERSION > VERSION
+    git add .
+    git commit -m "Auto generated gRPC"
+    git push origin HEAD
+    git tag -a -m "Auto generated version ${NEW_VERSION}." "v$NEW_VERSION"
+    git push origin --tags
+  else
+    echo "No changes detected for $1"
+  fi
+
+  leaveDir
+}
+
+buildAll
